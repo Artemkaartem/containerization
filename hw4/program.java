b521d91bc8e19fa04b6a55cb7442f80e295843df
artem <kuteinikovartemka@yandex.ru>
2023-10-12 16:44:45 +0300

    hw4

---
+++ hw4/program.java
@@ -0,0 +1,12 @@
+package hw4;
+
+import java.util.ArrayList;
+
+public class program {
+    public static void main(String[] args) {
+        ArrayList <String> list =new ArrayList<>();
+        list.add("Все");
+        list.add("работает");
+            System.out.println(list);
+    }
+}
